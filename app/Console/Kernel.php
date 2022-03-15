8879d652c85fd362fcaa74a4cba822fa75026b23
Ajay <ajayradadiya6333@gmail.com>
2022-03-15 13:01:48 +0530

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,13 +16,13 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param Schedule $schedule
      * @return void
      */
 
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('update:stockprice')->twiceDaily(1, 13)->timezone('America/New_York');
+         $schedule->command('update:stockprice')->hourly()->timezone('America/New_York');
     }
 
     /**

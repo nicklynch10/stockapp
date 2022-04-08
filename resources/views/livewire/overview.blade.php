4cf28e5c9c8defcafc6f0072b61f59b9f7c839b1
Ajay <ajayradadiya6333@gmail.com>
2022-04-08 16:03:16 +0530

    Change chart hover text and number format

--- resources/views/livewire/overview.blade.php
+++ resources/views/livewire/overview.blade.php
@@ -154,26 +154,7 @@
     </div>
     @endif
 
-
-{{--    @if(isset($this->box2) && count($this->box2)>0 && $totalTaxableGainLoss!=0)--}}
-{{--    <div class="container mx-auto px-4 py-10 md:py-12">--}}
-{{--        <div class="flex flex-col bg-white shadow-xl sm:rounded-lg px-4 py-4">--}}
-{{--            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 example">--}}
-{{--                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">--}}
-{{--                    <div class="w-full mb-5">--}}
-{{--                        <h2 class="font-semibold text-xl text-gray-800 leading-tight">--}}
-{{--                            {{ __('Taxable Gain / (Loss) Over Time') }}--}}
-{{--                        </h2>--}}
-{{--                    </div>--}}
-{{--                    <div class="overflow-hidden sm: rounded-lg table-align">--}}
-{{--                        <div id="google-line-chart" style="height: 600px; width: 100%;"></div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    @endif--}}
-
+    @if(isset($this->box2) && count($this->box2)>0 && $totalTaxableGainLoss!=0)
     <div class="container mx-auto px-4 py-10 md:py-12">
         <div class="flex flex-col bg-white shadow-xl sm:rounded-lg px-4 py-4">
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 example">
@@ -184,12 +165,13 @@
                         </h2>
                     </div>
                     <div class="overflow-hidden sm: rounded-lg table-align">
-                        <canvas id="sales-summary-today"></canvas>
+                        <canvas id="TaxableGain"></canvas>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    @endif
 
     {{-- Stock Sell Modal  --}}
     @livewire('stock-sell-modal')
@@ -232,20 +214,32 @@
                         data:yValues
                     }]
                 },
+                options: {
+                    tooltips: {
+                        callbacks: {
+                            label: function(tooltipItem, data) {
+                                var dataLabel = data.labels[tooltipItem.index];
+                                var value = ' : $' + data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].toLocaleString();
 
+                                if (Chart.helpers.isArray(dataLabel)) {
+                                    dataLabel = dataLabel.slice();
+                                    dataLabel[0] += value;
+                                } else {
+                                    dataLabel += value;
+                                }
+
+                                return dataLabel;
+                            }
+                        }
+                    }
+                }
             });
     </script>
-    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-        <script type="text/javascript">
-            google.charts.load('current', {'packages':['corechart']});
-            google.charts.setOnLoadCallback(drawChart);
-
-            function drawChart() {
-
-                var data = google.visualization.arrayToDataTable([
-                    ['Date', 'Cumulative Taxable Gain / (Loss) Over Time'],
+    <script>
+        var data = {
+            labels:
+                [
                     @php
                         foreach($this->date as $key=>$d)
                         {
@@ -259,28 +253,20 @@
                             }
                             if($taxable!=0)
                             {
-                                echo "['".\Carbon\Carbon::parse($d->date_of_transaction)->format('n/j/Y')."', $taxable],";
+                                echo "'".\Carbon\Carbon::parse($d->date_of_transaction)->format('n/j/Y')."',";
                             }
                         }
                     @endphp
-                ]);
-
-                var options = {
-                    title: '',
-                    curveType: 'function',
-                    legend: { position: 'top'}
-                };
-
-                var chart = new google.visualization.LineChart(document.getElementById('google-line-chart'));
-
-                chart.draw(data, options);
-            }
-        </script>
-
-        <script>
-            var data = {
-                labels:
-                    [
+                ],
+            datasets: [
+                {
+                    label: "Cumulative Taxable Gain / (Loss) Over Time : ",
+                    lineTension: 0,
+                    backgroundColor: "rgba(255,255,255,0)",
+                    borderColor: "rgba(108,108,108,1)",
+                    borderWidth: 2,
+                    pointBackgroundColor: "#000",
+                    data: [
                         @php
                             foreach($this->date as $key=>$d)
                             {
@@ -290,83 +276,82 @@
                                     if($d->date_of_transaction>=$t->date_of_transaction)
                                     {
                                         $taxable+=($t->share_price-$t->ave_cost)*($t->stock);
+                                        $value=round($taxable,2);
                                     }
                                 }
                                 if($taxable!=0)
                                 {
-                                    echo "'".\Carbon\Carbon::parse($d->date_of_transaction)->format('n/j/Y')."',";
+                                    echo "$value,";
                                 }
                             }
                         @endphp
                     ],
-                datasets: [
-                    {
-                        label: "Sales $",
-                        lineTension: 0,
-                        backgroundColor: "rgba(143,199,232,0.2)",
-                        borderColor: "rgba(108,108,108,1)",
-                        borderWidth: 1,
-                        pointBackgroundColor: "#535353",
-                        data: [
-                            @php
-                                foreach($this->date as $key=>$d)
-                                {
-                                    $total=0; $taxable=0;
-                                    foreach($this->tran as $t)
-                                    {
-                                        if($d->date_of_transaction>=$t->date_of_transaction)
-                                        {
-                                            $taxable+=($t->share_price-$t->ave_cost)*($t->stock);
-                                            $value=abs($taxable);
-                                        }
+                }
+            ],
+        };
+
+        //var myChart =
+        new Chart(document.getElementById('TaxableGain'), {
+            type: 'line',
+            data: data,
+            options: {
+                animation: false,
+                legend: {display: false},
+                maintainAspectRatio: false,
+                responsive: true,
+                responsiveAnimationDuration: 0,
+                tooltips: {
+                    callbacks: {
+                        label: function(tooltipItem, data) {
+                            let label = data.datasets[tooltipItem.datasetIndex].label || '';
+                            var datavalue = Math.round(tooltipItem.yLabel * 100) / 100;
+                            if(datavalue<0)
+                            {
+                                label += '($'+Math.abs(datavalue)+')';
+                            }
+                            else
+                            {
+                                label += '$'+datavalue;
+                            }
+                            return label;
+                        }
+                    }
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            fontColor: "#000",
+                            fontSize: 15,
+                            beginAtZero: true,
+                            callback: function(value, index, values) {
+                                if(parseInt(value) >= 1000){
+                                    if(value<0){
+                                        return '($' + Math.abs(value).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ')';
                                     }
-                                    if($taxable!=0)
-                                    {
-                                        echo "$taxable,";
+                                    else{
+                                        return '$' + Math.abs(value).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
                                     }
                                 }
-                            @endphp
-                        ]
-                    }
-                ]
-            };
-
-            //var myChart =
-            new Chart(document.getElementById('sales-summary-today'), {
-                type: 'line',
-                data: data,
-                options: {
-                    animation: false,
-                    legend: {display: false},
-                    maintainAspectRatio: false,
-                    responsive: true,
-                    responsiveAnimationDuration: 0,
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true,
-                                callback: function(value, index, values) {
-                                    if(parseInt(value) >= 1000){
-                                        if(value<0){
-                                            return '($' + Math.abs(value).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ')';
-                                        }
-                                        else{
-                                            return '$' + Math.abs(value).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                                        }
+                                else {
+                                    if(value<0){
+                                        return '($' + Math.abs(value) + ')';
                                     }
-                                    else {
-                                        if(value<0){
-                                            return '($' + Math.abs(value) + ')';
-                                        }
-                                        else{
-                                            return '$' + Math.abs(value);
-                                        }
+                                    else{
+                                        return '$' + Math.abs(value);
                                     }
                                 }
                             }
-                        }]
-                    }
+                        }
+                    }],
+                    xAxes: [{
+                        ticks: {
+                            fontColor: "#000",
+                            fontSize: 15,
+                            beginAtZero: true,
+                        }
+                    }]
                 }
-            });
-        </script>
+            }
+        });
+    </script>
 </main>

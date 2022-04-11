062595e4468c7208946f99dec7606f8ba0563679
Ajay <ajayradadiya6333@gmail.com>
2022-04-11 16:08:15 +0530

    Fix

--- resources/views/components/notification-card.blade.php
+++ resources/views/components/notification-card.blade.php
@@ -1,5 +1,5 @@
 <div class="mb-4 shadow bg-white rounded self-start">
-    <div class="grid grid-cols-12 flex items-center md:text-center border-gray-200 py-4 px-4 lg:px-6 border-b-2 space-x-2 w-full xs:flex xs:text-center xs:flex-wrap xs:justify-center">
+    <div class="grid grid-cols-12 flex items-center md:text-center border-gray-200 py-4 px-4 lg:px-6 border-b-2 space-x-2 w-full xs:flex-col xs:flex xs:text-center xs:flex-wrap xs:justify-center">
         <div class="col-span-2 col-end-2 xs:col-end-3">
             <a class="">
                 <img src="{{ $n->notifiable->profile_photo_url }}" class="rounded-full hover:bg-gray-100 h-10 w-auto">

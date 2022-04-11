62b496166d3277a72a638cbae4c4a4b1f2ffbc4a
ajayr6333 <99161382+ajayr6333@users.noreply.github.com>
2022-04-11 10:18:13 +0530

    Delete 01_set_log_file_permissions.sh
    not required

--- .platform/hooks/postdeploy/01_set_log_file_permissions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo chmod -R 777 /var/www/html/storage/*
-sudo chmod -R 777 /var/www/html/bootstrap/*
-sudo chmod -R 777 /var/app/current/storage/*
-sudo chmod -R 777 /var/app/current/bootstrap/*

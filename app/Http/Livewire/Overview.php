5ae88da2eedb94ba40a8a76bc1831027725f353c
Ajay <ajayradadiya6333@gmail.com>
2022-04-13 10:44:00 +0530

    Fix

--- app/Http/Livewire/Overview.php
+++ app/Http/Livewire/Overview.php
@@ -31,7 +31,7 @@ class Overview extends Component
         $this->sto = Stock::select('stock_ticker','current_share_price',DB::raw("(sum(share_number)) as total_stock"))->where('user_id',Auth::user()->id)->groupBy('stock_ticker','current_share_price')->get();
         $this->totalSavingsRealized=0;
 
-        $this->tran = Stock::join('transaction','transaction.stock_id','stock.id')->where('stock.user_id',Auth::user()->id)->get();
+        $this->tran = Stock::join('transaction','transaction.stock_id','stock.id')->where('stock.user_id',Auth::user()->id)->whereYear('stock.date_of_purchase', '=', date('Y'))->get();
         $this->date=Transaction::select('date_of_transaction')->where('user_id',Auth::user()->id)->groupBy('date_of_transaction')->get();
 
         //Box 2

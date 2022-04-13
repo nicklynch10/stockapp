f3a3d89859e10690e9337a88c6c92d604151e736
Ajay <ajayradadiya6333@gmail.com>
2022-04-13 14:32:54 +0530

    Image update

--- app/Http/Livewire/Overview.php
+++ app/Http/Livewire/Overview.php
@@ -28,9 +28,11 @@ class Overview extends Component
 
     public function render()
     {
+        //  Pie chart
         $this->sto = Stock::select('stock_ticker','current_share_price',DB::raw("(sum(share_number)) as total_stock"))->where('user_id',Auth::user()->id)->groupBy('stock_ticker','current_share_price')->get();
         $this->totalSavingsRealized=0;
 
+        // Line chart
         $this->tran = Stock::join('transaction','transaction.stock_id','stock.id')->where('stock.user_id',Auth::user()->id)->whereYear('stock.date_of_purchase', '=', date('Y'))->get();
         $this->date=Transaction::select('date_of_transaction')->where('user_id',Auth::user()->id)->whereYear('date_of_transaction', '=', date('Y'))->groupBy('date_of_transaction')->orderBy('date_of_transaction','ASC')->get();
 
@@ -42,6 +44,7 @@ class Overview extends Component
             $taxable+=($b2->share_price-$b2->ave_cost)*($b2->stock);
         }
         $this->totalTaxableGainLoss=$taxable;
+
         //Box 3
         $box3=Stock::join('view_stock_update','view_stock_update.stock_id','stock.id')->where('user_id',Auth::user()->id)->get();
         $nagative=0;
--- resources/views/support/help.blade.php
+++ resources/views/support/help.blade.php
@@ -12,48 +12,48 @@
 
                 @include('support.slider-small',['question'=>"How  to add Purchase New Stock?",
                'answer'=>" Head over to your portfolio page and click “Buy New Stock” button. And then Open Purchase New Stock modal,  add  your ticker or company name in input field like AMZN, Amazon.com see below screen short.
-                            <img src='/other/stock/new-stock.png'>
+                            <img src='/other/stock/new-stocks.png'>
                             User can get all the current data for this ticker And then save it.
            "])
 
                 @include('support.slider-small',['question'=>"How to Sell the stock?",
                'answer'=>" Head over to your portfolio page and click “SELL” button in stock record what you have sell stock
-                            <img src='/other/stock/sell-button.png'>
+                            <img src='/other/stock/sell-buttons.png'>
                            Open “Stock Sale” modal. In this modal add how many share you want to sell and what is the average price per share and then save it.
            "])
 
                 @include('support.slider-small',['question'=>"How to Add Existing Stock?",
                'answer'=>" Head over to your portfolio page and click “Buy” button in stock record what you have buy stock
-                            <img src='/other/stock/buy-button.png'>
+                            <img src='/other/stock/buy-buttons.png'>
                            Open “Buy Existing Stock” modal. In this modal add how many share you want to add and what is average price per share and then save it.
            "])
 
                 @include('support.slider-small',['question'=>"How to show all the stock “Buy” or “Sell” transaction?",
                'answer'=>" Head over to your portfolio page and show the “Historical Trades” table
-                            <img src='/other/stock/historical.png'>
+                            <img src='/other/stock/historicals.png'>
 
            "])
 
                 @include('support.slider-small',['question'=>"How can I edit the stock data?",
                'answer'=>" Head over to your portfolio page and click “Edit” icon in stock record what you have edit stock
-                            <img src='/other/stock/edit-icon.png'>
+                            <img src='/other/stock/edit-icons.png'>
                             Open “Purchase New Stock” modal. In this modal edit stock data what you want and then save it.
            "])
 
                 @include('support.slider-small',['question'=>"How to show stock ticker all the details?",
                'answer'=>" Head over to your portfolio page and show the stock data in main table click the particular stock company name and open “Company Detail” modal display all the detail of this stock
-                            <img src='/other/stock/company-detail.png'>
+                            <img src='/other/stock/company-details.png'>
            "])
 
                 @include('support.slider-small',['question'=>"How can create “New Account”",
                'answer'=>" Head over to your accounts page and click “Add New Account” button
-                            <img src='/other/account/account.png'>
+                            <img src='/other/account/accounts.png'>
                            Open “New Account” modal. In this modal add required and then save it.
            "])
 
                 @include('support.slider-small',['question'=>"Can I set account as default",
                'answer'=>" Yes, head over the accounts page>>show the record of the account, check the checkbox which you want to set as a default
-                            <img src='/other/account/set-as-default.png'>
+                            <img src='/other/account/set-as-defaults.png'>
            "])
 
             </div>

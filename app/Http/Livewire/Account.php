8e4be994303324791442d329809761b477fb69fa
Ajay <ajayradadiya6333@gmail.com>
2022-04-19 17:52:31 +0530

    Plaid integrate

--- app/Http/Livewire/Account.php
+++ app/Http/Livewire/Account.php
@@ -6,6 +6,7 @@ use App\Models\Stock;
 use App\Models\StockTicker;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 use App\Models\Account as Accounts;
 
@@ -26,6 +27,21 @@ class Account extends Component
 
     public function render()
     {
+        $url = "https://sandbox.plaid.com/investments/holdings/get";
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $headers = array(
+            "Content-Type: application/json",
+        );
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        $data = '{"client_id":"6256b65fccf78000158a06aa","secret":"ade346c8009da99dea064fb6075270","access_token":"access-sandbox-ecb7bc4d-f13a-476b-a317-801e2088ae8d"}';
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        $resp = curl_exec($curl);
         $this->account=Accounts::where('user_id', Auth::user()->id)->orderBy('account.created_at', 'DESC')->get();
         return view('livewire.account');
     }

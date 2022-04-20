986bcce27eda8e3aa0a88ad799a30286768456f5
Ajay <ajayradadiya6333@gmail.com>
2022-04-20 11:57:40 +0530

    Fix

--- resources/views/livewire/stock.blade.php
+++ resources/views/livewire/stock.blade.php
@@ -209,22 +209,22 @@
     </x-jet-confirmation-modal>
 
 
-{{--    <x-jet-confirmation-modal wire:model="issellShareOpen">--}}
-{{--        <x-slot name="title">--}}
-{{--            {{ __('Enter valid share') }}--}}
-{{--        </x-slot>--}}
+    <x-jet-confirmation-modal wire:model="issellShareOpen">
+        <x-slot name="title">
+            {{ __('Enter valid share') }}
+        </x-slot>
 
-{{--        <x-slot name="content">--}}
-{{--            <span class="text-red-700">{{ __('You can never sell more shares than you have') }}</span>--}}
-{{--        </x-slot>--}}
+        <x-slot name="content">
+            <span class="text-red-700">{{ __('You can never sell more shares than you have') }}</span>
+        </x-slot>
 
-{{--        <x-slot name="footer">--}}
-{{--            <x-jet-secondary-button wire:click="closeSellShareModal()">--}}
-{{--                {{ __('Cancel') }}--}}
-{{--            </x-jet-secondary-button>--}}
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="closeSellShareModal()">
+                {{ __('Cancel') }}
+            </x-jet-secondary-button>
 
-{{--        </x-slot>--}}
-{{--    </x-jet-confirmation-modal>--}}
+        </x-slot>
+    </x-jet-confirmation-modal>
 
 </main>
 

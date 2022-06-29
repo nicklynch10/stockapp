6d98841759e166b3128328c397c91a918592e7d0
Ajay <ajayradadiya6333@gmail.com>
2022-06-29 17:42:36 +0530

    Change migration

--- database/migrations/2022_06_29_041542_add_index_to_cryptocurrency_table.php
+++ database/migrations/2022_06_29_041542_add_index_to_cryptocurrency_table.php
@@ -15,7 +15,7 @@ class AddIndexToCryptocurrencyTable extends Migration
     {
         Schema::table('cryptocurrency', function (Blueprint $table) {
             $table->string('crypto_name')->change();
-            $table->index('id');
+//            $table->index('id');
             $table->index('crypto_symbol');
             $table->index('crypto_name');
         });
@@ -29,7 +29,7 @@ class AddIndexToCryptocurrencyTable extends Migration
     public function down()
     {
         Schema::table('cryptocurrency', function (Blueprint $table) {
-            $table->dropIndex(['id','crypto_symbol','crypto_name']);
+            $table->dropIndex(['crypto_symbol','crypto_name']);
         });
     }
 }
